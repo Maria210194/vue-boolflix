86d8cf8c099902d282787573484eab25d56a3b32
Maria Stachie <mary.stachie@gmail.com>
2022-04-20 23:21:43 +0200

    search

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <HeaderComponent />
+    <HeaderComponent @ricerca="queryApi" />
     <MainComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import HeaderComponent from "./components/HeaderComponent.vue";
 import MainComponent from "./components/MainComponent.vue";
-
 import axios from "axios";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
     HeaderComponent,
     MainComponent,
   },
   data() {
     return {
-      apiUrl: "https://developers.themoviedb.org/3/search/",
+      apiUrl: "https://api.themoviedb.org/3/search/",
       apiKey: "4efb87d52a46e108a98dadb3dd2051f1",
+      arraySerie: [],
+      arrayFilm: [],
     };
   },
-  mounted() {
-    axios.get(this.apiUrl + "tv");
+  methods: {
+    queryApi(cerca) {
+      if (cerca !== "") {
+        const params = {
+          query: cerca,
+          api_key: this.apiKey,
+          language: "it-IT",
+        };
+
+        // chiamata:
+        axios
+          .get(this.apiUrl + "tv", { params })
+          .then((response) => {
+            console.log(response.data.results);
+            this.arraySerie = response.data.results;
+            console.log(this.arraySerie);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,10 +1,38 @@
 <template>
-  <div>header</div>
+  <div>
+    <input type="text" v-model="query" />
+    <button @click="queryApi">Search</button>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "HeaderComponent",
+  data() {
+    return {
+      apiUrl: "https://api.themoviedb.org/3/search/",
+      apiKey: "4efb87d52a46e108a98dadb3dd2051f1",
+
+      query: "",
+    };
+  },
+  methods: {
+    queryApi() {
+      const params = {
+        query: this.query,
+        api_key: this.apiKey,
+      };
+      axios
+        .get(this.apiUrl + "movie", { params })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 
